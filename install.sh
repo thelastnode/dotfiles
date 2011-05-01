010173807368412d539ed811c1f72cbb9ce2f186
Ankit Shankar <ankitshankar@gmail.com>
2011-05-01 02:29:05 -0400

    Install script changes for vim stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# vim stuff
 ln -s vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload/
+cd ~/.vim/autoload/
+wget https://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim
+cd -
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git init
+git submodule add git@github.com:flazz/vim-colorschemes.git colorschemes
+git submodule add https://github.com/Raimondi/delimitMate.git
+cd -
+
 ln -s gitconfig ~/.gitconfig
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
