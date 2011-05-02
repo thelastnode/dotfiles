32de0529263782879e727bac07b50994e5b6ea5e
Ankit Shankar <ankitshankar@gmail.com>
2011-05-01 20:56:11 -0400

    Update xmonad.hs for xmobar
    Disclaimer: no longer using XMonad as my primary window manager.

--- xmonad.hs
+++ xmonad.hs
@@ -6,6 +6,20 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
+    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
         { modMask = mod4Mask
-        }
+        , borderWidth = 2
+        , terminal = "gnome-terminal"
+        , focusFollowsMouse = False
+ 
+        -- For xmobar:
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        ]
